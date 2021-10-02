2a6f93b8374cfc0faf07f383d60687c3780a1af0
RogueArt <altoraries@gmail.com>
2021-10-01 22:29:11 -0700

    Added example env as well as bot starter code

---
+++ src/app.js
@@ -0,0 +1,52 @@
+// Allow access to .env file
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Get keys from .env
+const { BOT_TOKEN } = process.env
+
+// Discord
+import { Client, Intents } from 'discord.js'
+
+// Initialize Discord client
+const client = new Client({ intents: [Intents]})
+
+// Bring in commands, helper functions
+import { extractCmdAndArgs, isValidMsg } from './utils/lib'
+import { hello } from './commands/index.js'
+
+// Execute on start up
+client.on('ready', () => {
+  console.log('Bot name', 'bot started!')
+})
+
+// Execute on message creation
+client.on('messageCreate', async msg => {
+  // Validate message if needed
+  if (!isValidMsg(msg)) {
+    msg.channel.send({ content: 'Invalid message.' })
+    return
+  }
+
+  // Log commands for later use
+  // logCmd(msg)
+
+  // Get command name and arguments from the message
+  const [cmd, args] = extractCmdAndArgs(msg) 
+
+  // Call right function for command
+  switch (cmd) {
+    // 
+    case 'hello':
+      await hello(msg, args[0])
+      break
+    
+
+    // Default command
+    default:
+        break
+  }
+})
+
+// Log the bot into Discord
+client.login(BOT_TOKEN)
