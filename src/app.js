e0cfee166ad78ba3ad2e05761ab70c2e1416c306
RogueArt <altoraries@gmail.com>
2021-11-03 22:56:40 -0700

    Ran formatted on code; improved message handling and validation

--- src/app.js
+++ src/app.js
@@ -3,40 +3,42 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // Get keys from .env
-const { BOT_TOKEN } = process.env
+const { BOT_TOKEN, CMD_PREFIX } = process.env
 
 // Discord
 import { Client, Intents } from 'discord.js'
 
 // Initialize Discord client
-const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+})
 
 // Bring in commands, helper functions
-import { extractCmdAndArgs, isValidMsg, logCmd } from './utils/lib'
+import { extractCmdAndArgs, isValidMsg, logCmd } from './utils/lib.js'
 import { hello, help } from './commands/index.js'
 
 // Execute on start up
 client.on('ready', () => {
-  console.log('Bot name', 'bot started!')
+  console.log('Your bot name here', 'bot started!')
 })
 
 // Execute on message creation
 client.on('messageCreate', async msg => {
+  // Don't reply to message sent by bot
+  if (msg.author.bot) return
+
   // Validate message if needed
-  if (!isValidMsg(msg)) {
-    msg.channel.send({ content: 'Invalid message.' })
-    return
-  }
+  if (!isValidMsg(msg)) return // msg.channel.send('Invalid message')
 
   // Store logs of commands by users
   logCmd(msg)
 
   // Get command name and arguments from the message
-  const [cmd, args] = extractCmdAndArgs(msg) 
+  const [cmd, args] = extractCmdAndArgs(msg)
 
   // Call right function for command
   switch (cmd) {
-    // Hello world command 
+    // Hello world command
     case 'hello':
       await hello(msg, args)
       break
@@ -44,10 +46,13 @@ client.on('messageCreate', async msg => {
     case 'help':
       await help(msg, args)
       break
-    
+
     // Default handler if command not understood
     default:
-        break
+      msg.channel.send(
+        `Invalid command. Type \`${CMD_PREFIX}help\` to see the list of commands.`
+      )
+      break
   }
 })
 
